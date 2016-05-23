8630fdeca5fcb7461fdfe9842abe4b828bc96d8d
Imane <iloukili11@gmail.com>
2016-05-23 23:28:17 +0000

    Class Solution Start

---
+++ BSA/src/Solution.java
@@ -0,0 +1,16 @@
+import java.util.Vector;
+
+public class Solution {
+	
+	public Vector<Double>TabLine=new Vector<Double>();
+	private double currentFitness;
+	private int dim;
+	private Problem pbm;
+	public Solution(Problem pbm) {
+		super();
+		this.pbm = pbm;
+	}
+	
+	
+	
+}
