98c13aac1f02043342eea957816ea8cfa1c89f87
Hasnaa <my-crazy-dream@hotmail.fr>
2016-05-24 21:42:11 +0100

    main

---
+++ BSA/src/Metier/Test.java
@@ -0,0 +1,41 @@
+package Metier;
+
+import java.util.Scanner;
+
+public class Test {
+
+	public static void SelectParam(){
+		System.out.println("Entrez ces parametres :");
+		System.out.println("	- dimension (la taille d'un individu)" +"\t \t"+ "	- popsize (le nombre d'individus de la population)");
+		System.out.println("	- indRun (le nombre d'executions)"+"\t \t"+"        - mixrate (la probabilite de mutation)");
+		
+	
+	}
+	public static void SelectFunction(){
+		System.out.println("choisisez la fonction de Benchmark a executer : ");
+		System.out.println("	1 - Rosenbrock"+"\t \t"+  "	2 - Rastragin" +"\t \t"+    "3 - Ackley");
+		System.out.println("	4 - Schwefel" +"\t \t "+	"5 - schaffer" +"\t \t "+	"6 - Weierstrass");
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		SelectParam();
+		
+		Configuration conf=new Configuration();
+		Scanner sc=new Scanner(System.in);
+		
+		
+		
+		System.out.println(conf);
+		
+		SelectFunction();
+		Problem pbm=new Problem();
+		System.out.println("Resultat obtenu sur le nombre d'execution :");
+		MyAlgorithm algo=new MyAlgorithm(conf, pbm);
+	
+
+	}
+
+}
